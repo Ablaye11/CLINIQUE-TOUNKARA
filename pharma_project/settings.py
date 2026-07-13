043a191229da04d093198d8ca405207537fe0587
Your Name <you@example.com>
2026-07-13 14:44:48 +0000

    Fix mobile POS responsive layout and update deployment settings

--- pharma_project/settings.py
+++ pharma_project/settings.py
@@ -19,7 +19,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'clinique-tounkara-pharma-secre
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get(
+    'DJANGO_ALLOWED_HOSTS',
+    '127.0.0.1,localhost,cliniquetounkara.pythonanywhere.com'
+).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
